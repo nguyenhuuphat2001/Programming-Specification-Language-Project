e139eda004d61d02f2828a652e6ea15ef9465be8
nguyenhuuphat2001 <68334213+nguyenhuuphat2001@users.noreply.github.com>
2021-10-12 09:11:32 +0700

    Revert "Revert "New""
    This reverts commit 7e2e8bfb3f0b4fa09e63f5d8e66bc80a4bf28e31.

---
+++ Formal_Specification/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Formal_Specification
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Location = new System.Drawing.Point(248, 21);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(75, 23);
+            this.btnBuild.TabIndex = 2;
+            this.btnBuild.Text = "Build";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(337, 288);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox2.Location = new System.Drawing.Point(344, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(549, 449);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(248, 48);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 450);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnBuild);
+            this.Name = "Form1";
+            this.Text = "Convert";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ Formal_Specification/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.IO;
+
+namespace Formal_Specification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnBuild.Click += BtnBuild_Click;
+            btnRun.Click += BtnRun_Click;
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ICodeCompiler icc = codeProvider.CreateCompiler();
+            string Output = "Out.exe";
+            Button ButtonObject = (Button)sender;
+
+            //textBox2.Text = "";
+            System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+            //Make sure we generate an EXE, not a DLL
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = Output;
+            CompilerResults results = codeProvider.CompileAssemblyFromSource(parameters, richTextBox2.Text);
+
+            if (results.Errors.Count > 0)
+            {
+                /*textBox2.ForeColor = Color.Red;
+                foreach (CompilerError CompErr in results.Errors)
+                {
+                    textBox2.Text = textBox2.Text +
+                                "Line number " + CompErr.Line +
+                                ", Error Number: " + CompErr.ErrorNumber +
+                                ", '" + CompErr.ErrorText + ";" +
+                                Environment.NewLine + Environment.NewLine;
+                }*/
+            }
+            else
+            {
+                //Successful Compile
+    /*            textBox2.ForeColor = Color.Blue;
+                textBox2.Text = "Success!";
+                //If we clicked run then launch our EXE
+                if (ButtonObject.Text == "Run") Process.Start(Output);*/
+                Process.Start(Output);
+            }
+
+        }
+
+        private void BtnBuild_Click(object sender, EventArgs e)
+        {
+            Function function= new Function();
+            function.splitFunction(richTextBox1.Text);
+            richTextBox2.Text = function.printFunction();
+            string[] words = { "public", "void","int","float","double","ref"};
+            foreach(string word in words)
+            {
+                int startIndex = 0;
+                while(startIndex < richTextBox2.TextLength)
+                {
+                    int wordStartIndex = richTextBox2.Find(word, startIndex, RichTextBoxFinds.None);
+                    if (wordStartIndex != -1)
+                    {
+                        richTextBox2.SelectionStart = wordStartIndex;
+                        richTextBox2.SelectionLength = word.Length;
+                        richTextBox2.SelectionColor = Color.Blue;
+                    }
+                    else
+                        break;
+                    startIndex += wordStartIndex + 1;
+                }
+            }
+        }
+
+
+    }
+}
